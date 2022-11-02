bd1e893a3d6b8b39f7bc70c8f9b16f8dbde397fa
Bakteriachan <josuegarcia17904@gmail.com>
2022-11-02 13:57:52 -0400

    added CORS

--- posts/index.js
+++ posts/index.js
@@ -2,11 +2,13 @@ import express from "express";
 
 import network from "./network.js";
 import config from "../config/index.js";
+import cors from "cors";
 import errors from "../middlewares/errors.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 
 app.use('/post', network);
 
